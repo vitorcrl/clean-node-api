e3bf6f3ae88e169f9bd1583701dc13d8428e6510
Vitor Carlos <vitorcarlos1312@gmail.com>
2022-10-20 22:52:16 -0300

    refactor: refact server error to helpers

--- src/presentation/helpers/http-helper.ts
+++ src/presentation/helpers/http-helper.ts
@@ -4,3 +4,7 @@ export const badRequest = (error: Error): httpResponse => ({
   statusCode: 400,
   body: error,
 });
+export const serverError= (): httpResponse => ({
+  statusCode: 500,
+  body: new Error("Internal server error"),
+})
