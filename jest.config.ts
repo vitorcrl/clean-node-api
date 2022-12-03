2f377a526533c3eb54a942c8c016f455c026975b
Vitor Carlos <vitorcarlos1312@gmail.com>
2022-12-03 17:31:46 -0300

    feat: adjust test ci

--- jest.config.ts
+++ jest.config.ts
@@ -1,7 +1,13 @@
 module.exports = {
     roots: ['<rootDir>/src'],
     collectCoverage: true,
-    collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+    collectCoverageFrom: [
+        '<rootDir>/src/**/*.ts',
+        '!<rootDir>/src/main/**',
+        '!<rootDir>/src/domain/**',
+        '!<rootDir>/src/data/**',
+        '!<rootDir>/src/presentation/**',
+    ],
     coverageDirectory: 'coverage',
     coverageProvider: 'v8',
     testEnvironment: 'node',
