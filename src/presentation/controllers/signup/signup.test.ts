9055c587bee1ffbe712efc9593dbdfc7103a3730
Vitor Carlos <vitorcarlos1312@gmail.com>
2022-12-21 20:08:38 -0300

    refactor: make server error return the error stack

--- src/presentation/controllers/signup/signup.test.ts
+++ src/presentation/controllers/signup/signup.test.ts
@@ -176,7 +176,7 @@ describe('Signup Controller', () => {
 
         const httpResponse = await sut.handle(httpRequest)
         expect(httpResponse.statusCode).toBe(500)
-        expect(httpResponse.body).toEqual(new ServerError())
+        expect(httpResponse.body).toEqual(new ServerError(''))
     })
 
     test('Should call AddAccount with correct values', async () => {
@@ -214,7 +214,7 @@ describe('Signup Controller', () => {
 
         const httpResponse = await sut.handle(httpRequest)
         expect(httpResponse.statusCode).toBe(500)
-        expect(httpResponse.body).toEqual(new ServerError())
+        expect(httpResponse.body).toEqual(new ServerError(''))
     })
     test('Should return 200 if valid data is provided', async () => {
         const { sut } = makeSut()
--- src/presentation/helpers/http-helper.ts
+++ src/presentation/helpers/http-helper.ts
@@ -1,15 +1,15 @@
-import { ServerError } from "../errors";
-import { httpResponse } from "../protocols/http";
+import { ServerError } from '../errors'
+import { httpResponse } from '../protocols/http'
 
 export const badRequest = (error: Error): httpResponse => ({
-  statusCode: 400,
-  body: error,
-});
-export const serverError= (): httpResponse => ({
-  statusCode: 500,
-  body: new ServerError(),
+    statusCode: 400,
+    body: error,
+})
+export const serverError = (error: Error): httpResponse => ({
+    statusCode: 500,
+    body: new ServerError(error.stack),
 })
 export const ok = (data: any): httpResponse => ({
-  statusCode: 200,
-  body: data,
-});
+    statusCode: 200,
+    body: data,
+})
