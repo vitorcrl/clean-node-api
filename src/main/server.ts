213acdb8c641e6b7eea523628492cb0129899fbc
Vitor Carlos <vitorcarlos1312@gmail.com>
2023-01-05 23:34:44 -0300

    refactor: updated message

--- src/main/server.ts
+++ src/main/server.ts
@@ -5,7 +5,8 @@ MongoHelper.connect(env.mongoUrl)
     .then(async () => {
         const app = (await import('./config/app')).default
         app.listen(env.port, () =>
-            console.log(`Server is running at http://localhost:${env.mongoUrl}`)
+            console.log(`Server is running at http://localhost:${env.mongoUrl}
+            http://localhost:5050/api/signup`)
         )
     })
     .catch(console.error)
