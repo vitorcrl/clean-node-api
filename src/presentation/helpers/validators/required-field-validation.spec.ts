a1bfeb5c162dac8ef5e1373e218e8dc5e4d824f1
Vitor Carlos <vitorcarlos1312@gmail.com>
2023-03-26 22:10:11 -0300

    test: ensure required field validation not returns any

--- src/presentation/helpers/validators/required-field-validation.spec.ts
+++ src/presentation/helpers/validators/required-field-validation.spec.ts
@@ -7,4 +7,10 @@ describe('RequiredFieldValidation', () => {
         const error = sut.validate({ name: 'any_name' })
         expect(error).toEqual(new MissingParamError('any_field'))
     })
+    //nao retornar nada se der certo, ou seja, se o campo for preenchido
+    test('Should not return if validation succeeds', () => {
+        const sut = new RequiredFieldValidation('any_field')
+        const error = sut.validate({ any_field: 'any_name' })
+        expect(error).toBeFalsy()
+    })
 })
