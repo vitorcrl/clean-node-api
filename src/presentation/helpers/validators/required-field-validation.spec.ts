50785f3f1592106fbef780eec72ec84ac301c4c1
Vitor Carlos <vitorcarlos1312@gmail.com>
2023-03-26 22:11:26 -0300

    refactor: refact sut

--- src/presentation/helpers/validators/required-field-validation.spec.ts
+++ src/presentation/helpers/validators/required-field-validation.spec.ts
@@ -1,15 +1,18 @@
 import { MissingParamError } from '../../errors'
 import { RequiredFieldValidation } from './required-field-validation'
 
+const makeSut = (): RequiredFieldValidation => {
+    return new RequiredFieldValidation('any_field')
+}
 describe('RequiredFieldValidation', () => {
     test('Should return a MissingParamError if validation fails', () => {
-        const sut = new RequiredFieldValidation('any_field')
+        const sut = makeSut()
         const error = sut.validate({ name: 'any_name' })
         expect(error).toEqual(new MissingParamError('any_field'))
     })
     //nao retornar nada se der certo, ou seja, se o campo for preenchido
     test('Should not return if validation succeeds', () => {
-        const sut = new RequiredFieldValidation('any_field')
+        const sut = makeSut()
         const error = sut.validate({ any_field: 'any_name' })
         expect(error).toBeFalsy()
     })
