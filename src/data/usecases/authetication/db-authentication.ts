4c1fd618d41d639633eb5e499e7f4bc9d49bf017
Vitor Carlos <vitorcarlos1312@gmail.com>
2023-04-16 17:42:00 -0300

    feat: ensure DbAuthentication calls HashCopare with correct informations

--- src/data/usecases/authetication/db-authentication.ts
+++ src/data/usecases/authetication/db-authentication.ts
@@ -2,19 +2,28 @@ import {
     Authentication,
     IAuthenticationModel,
 } from '../../../domain/usecases/authentication'
+import { IHashCompare } from '../../protocols/cryptography/hash-compare'
 import { LoadAccountByEmailRepository } from '../../protocols/db/load-account-by-email-repository'
 
 export class DbAuthentication implements Authentication {
     constructor(
-        private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository
+        private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository,
+        private readonly hashCOmpare: IHashCompare
     ) {
         this.loadAccountByEmailRepository = loadAccountByEmailRepository
+        this.hashCOmpare = hashCOmpare
     }
 
     async auth(authentication: IAuthenticationModel): Promise<string> {
-        await this.loadAccountByEmailRepository.loadByEmail(
+        const account = await this.loadAccountByEmailRepository.loadByEmail(
             authentication.email
         )
+        if (account) {
+            await this.hashCOmpare.compare(
+                authentication.password,
+                account.password
+            )
+        }
         return null
     }
 }
